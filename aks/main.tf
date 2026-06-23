e6a2518d0bd6acbbd298dffdd5d74f240c3369b1
sukhjiwan <sukhjiwansinghaulakh@b18g12.online>
2026-06-23 11:30:57 +0000

    update

--- aks/main.tf
+++ aks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "exampleaks1"
-
+####
   default_node_pool {
     name       = "default"
     node_count = 1
